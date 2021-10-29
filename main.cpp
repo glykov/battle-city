bbc68208ccb86fe75ef2ea6f7376ad84eef78ad8
glykov <glebdom@gmail.com>
2021-10-29 17:18:10 +0300

    added second and third lines

--- main.cpp
+++ main.cpp
@@ -2,5 +2,8 @@
 
 int main()
 {
-	std::cout << "Hello Git!\n";
+	std::cout << "Hello Git!" << std::endl;
+	std::cout << "Second line" << std::endl;
+	std::cout << "Third line" << std::endl;
+	return 0;
 }
